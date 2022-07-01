04f6df1467e4fd0bafe8f41880afed4e153124b6
ateka-p <atekapeter@gmail.com>
2022-07-01 13:00:04 -0700

    unix is basically

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,16 @@
 #include <inistd.h>
 
 /**
- * main - prints exactly "and that piece of art is useful"
- * -Peter Ateka,
- *  followed by a new line, to the standard error
- * Return: 1 if success
+ * main - prints quote
+ * Description: prints Dors'snquote
+ *
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
-	return (1)
+
+write(2, "and that piece of art is useful\" - Dora Korapar, 2015-10-19\n", 59);
+	return (1);
 }
 
