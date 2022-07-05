b80a51bdd01223d52054fa0a0914a65c3ec44848
ateka-p <atekapeter@gmail.com>
2022-07-05 12:50:01 -0700

    I sometimes suffer

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet of letters
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	fir (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
