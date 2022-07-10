1b9a2f324abe4dc280065e1a169fa8be9a9c900f
ateka-p <atekapeter@gmail.com>
2022-07-09 19:19:16 -0700

    You are so much sunshine

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * print_square - prints a sqaure, followed by a new line;
+ * print_square - prints a square, followed by a new line;
  * @size: size of the square
  */
 void print_square(int size)
 {
 	if (size <= 0)
 	{
-		_putchar('\');
+		_putchar('\n');
 	} else
 	{
 		int i, j;
