3b29df7db1f9c2b417cce3ce3f9a462e0ffcfdc0
ateka-p <atekapeter@gmail.com>
2022-07-14 07:33:17 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int len = 0, i;
 
-	i = 0;
-	j = 0;
+	while (dest[len])
+		len++;
 
-	while (dest[i] != '\0')
+	for (i = 0; src[i] != 0; i++)
 	{
-		i++;
+		dest[len] = src[i];
+		len += 1;
 	}
-	while (src[j] != '\0')
-	{
-		dest[i] = src[j];
-		j++;
-		i++;
-	}
-
-	dest[i] = '\0';
+	dest[len] = '\0';
 	return (dest);
 }
